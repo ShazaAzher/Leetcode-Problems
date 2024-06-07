20d533972c2b00f2aed763449aa431400534cb38
Shaza Azher <58635784+ShazaAzher@users.noreply.github.com>
2024-06-07 15:05:17 +0500

    Create longest_substring_without_repeating_characters.py

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        max_length = 0
+        char_set = set()
+        left, right = 0, 0
+        
+        while right < n:
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                max_length = max(max_length, right - left + 1)
+                right += 1
+            else:
+                char_set.remove(s[left])
+                left += 1
+                
+        return max_length
+        
